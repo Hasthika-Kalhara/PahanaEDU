d42c1445bedd180fe7489dd1b586b68f334a0a05
kalhara Silva <74489380+Yasobiii@users.noreply.github.com>
2025-07-09 14:17:34 +0530

    git test 2

--- src/main/java/org/web/pahanaedu/HelloController.java
+++ src/main/java/org/web/pahanaedu/HelloController.java
@@ -1,3 +1,15 @@
 package org.web.pahanaedu;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController
+{
+    @RequestMapping("/hello")
+    public String hello()
+    {
+        return "Hello World!";
+    }
+    //Testing
 }
