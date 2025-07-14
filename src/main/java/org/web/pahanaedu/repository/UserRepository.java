473f13cef6f054f8fa33c0ea8142c49326aa542c
kalhara Silva <74489380+Hasthika-Kalhara@users.noreply.github.com>
2025-07-14 11:22:23 +0530

    Delete src directory

--- src/main/java/org/web/pahanaedu/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package org.web.pahanaedu.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.web.pahanaedu.model.User;
-
-public interface UserRepository extends JpaRepository<User, String>
-{
-    User findByUsername(String username);
-}
